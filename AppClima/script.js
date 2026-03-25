e5b94ce46f0a10677868ca62c66fa46f3efccad3
Enzo Rossi <enzorossifs@gmail.com>
2026-03-25 14:49:18 -0300

    Melhorando Lógica e arrumando detalhes

--- AppClima/script.js
+++ AppClima/script.js
@@ -5,7 +5,9 @@ button.addEventListener('click', function(event){
 
     const cityInput = document.getElementById('cityInput').value;
     const apiKey = '0120f7b8c0f55c18af41773179b1a148'; 
+    const weatherResult = document.getElementById('weatherResult');
 
+    weatherResult.innerHTML = "Buscando...";
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric&lang=pt_br`)
         .then(response => {
             if (!response.ok) {
@@ -14,7 +16,6 @@ button.addEventListener('click', function(event){
             return response.json();
         })
         .then(dados => {
-            const weatherResult = document.getElementById('weatherResult');
             weatherResult.innerHTML = '';
             if (dados && dados.main && dados.weather && dados.weather.length > 0) {
                 weatherResult.innerHTML = `
@@ -25,7 +26,7 @@ button.addEventListener('click', function(event){
                         <p>Tempo: ${dados.weather[0].description}</p>
                         <p>Humidade: ${dados.main.humidity}%</p>
                         <p>Pressão Atmosférica: ${dados.main.pressure} hPa</p>
-                        <p>Velocidade do Vento: ${dados.wind.speed}km/h</p>
+                        <p>Velocidade do Vento: ${dados.wind.speed}m/s</p>
                     </div>
                 `;
             } else {
@@ -34,7 +35,8 @@ button.addEventListener('click', function(event){
         })
     .catch(erro => {
         console.error("Erro na Requisição: ", erro);
-        const weatherResult = document.getElementById('weatherResult');
-        weatherResult.innerHTML = `<p>Erro ao buscar os dados. Verifique o nome da cidade.</p>`;
+        if (erro.message.includes("404")) {
+            weatherResult.innerHTML = "Cidade não encontrada...";
+        }   
     });
 });
